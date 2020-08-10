af5b5b87a7931124637892953244c07e18e2ea86
FilipeAVNunes <filipenunes407@hotmail.com>
2020-08-10 16:48:09 +0100

    Updates

---
+++ precache-manifest.8e2357417e26bd5c2b991392bd0ccf1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9934d3c2e174068246d669ae434af20",
+    "url": "/udemy-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0decffe068bff9a326a5",
+    "url": "/udemy-monsters-rolodex/static/css/main.acdb831a.chunk.css"
+  },
+  {
+    "revision": "650e2e23ef434db99bd8",
+    "url": "/udemy-monsters-rolodex/static/js/2.be633cc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-monsters-rolodex/static/js/2.be633cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0decffe068bff9a326a5",
+    "url": "/udemy-monsters-rolodex/static/js/main.7dbb0f5f.chunk.js"
+  },
+  {
+    "revision": "1675748f2329e185ed06",
+    "url": "/udemy-monsters-rolodex/static/js/runtime-main.a7e24b7d.js"
+  }
+]);
